ea8ba2e925e1c80b5ac19253b6d8aec17cc7a0da
PurelyStupidity <micoolplays@gmail.com>
2024-10-17 00:59:36 +1300

    lets gooo

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
 from textual.app import App, ComposeResult
 from textual.binding import Binding
 from textual.screen import Screen
-from textual.containers import Center, Middle
+from textual.widget import Widget
+from textual.containers import Container, Center, Middle, Horizontal, Vertical
 from textual.widgets import Footer, Placeholder, ProgressBar
+from textual.reactive import reactive
 
+class Current_Time_In_Track(Widget):
+    current_time = reactive("track_time")
+    def render(self) -> str:
+        return "0:00"
 
+class Track_Duration(Widget):
+    track_duration = reactive("track_time")
+    def render(self) -> str:
+        return "3:00"
 class Main_Screen(Screen):
     """The main page that contains:
     Main side bar
     Changelog
     Footer
     Playing bar"""
+
+    CSS_PATH = "main_page.tcss"
+
     def compose(self) -> ComposeResult:
-        with Center():
-            with Middle():
-                yield ProgressBar(total=30)
-                yield Footer()
+        yield Placeholder("top_bar", id="top_bar")
+        yield Placeholder("PLAYLISTS", id="sidebar")
+        yield Placeholder("MAIN PAGE", id="main_page")
+        yield Container(Placeholder("CONTROLS", id="controls"), id="Bottom")
+        with Center(id="bar_container"):
+            # yield Current_Time_In_Track()
+            yield ProgressBar(
+                    total=100, id="bar", show_percentage=False, show_eta=False
+                )
+            # yield Track_Duration()
+
 
 class MainApp(App):
     def on_mount(self) -> None:
         self.install_screen(Main_Screen(), "main")
         self.push_screen("main")
+
 
 if __name__ == "__main__":
     app = MainApp()
