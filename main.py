4b655ad63849b673d694982d6546e4cdf804a1df
Arthur Mckellar <micoolplays@gmail.com>
2024-10-17 01:37:24 +0000

    Oct 17, 2024, 2:37 PM

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class Main_Screen(Screen):
         yield Placeholder("MAIN PAGE", id="main_page")
         yield Container(Placeholder("CONTROLS", id="controls"), id="Bottom")
         yield Container(Current_Time_In_Track(),
-                        Middle(ProgressBar(total=100, id="bar"), show_percentage=False, show_eta=False), Track_Duration(),id="bar_container")
+                        Center(ProgressBar(total=100, id="bar", show_percentage=False, show_eta=False)), Track_Duration(),id="bar_container")
 
 
 class MainApp(App):
