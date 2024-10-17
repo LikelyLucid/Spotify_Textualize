1ee276e3666a4d430e0f83f35aae94afa1e08c5b
Arthur Mckellar <micoolplays@gmail.com>
2024-10-17 01:57:50 +0000

    Oct 17, 2024, 2:57 PM

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from textual.binding import Binding
 from textual.screen import Screen
 from textual.widget import Widget
 from textual.containers import Container, Center, Middle, Horizontal, Vertical
-from textual.widgets import Footer, Placeholder, ProgressBar
+from textual.widgets import Footer, Placeholder, ProgressBar, Button
 from textual.reactive import reactive
 
 class Current_Time_In_Track(Widget):
@@ -15,6 +15,12 @@ class Track_Duration(Widget):
     track_duration = reactive("track_time")
     def render(self) -> str:
         return "3:00"
+
+class Current_Track(Widget):
+    current_track = reactive("track")
+    def render(self) -> str:
+        return "TRACK PLACEHOLDER"
+
 class Main_Screen(Screen):
     """The main page that contains:
     Main side bar
@@ -30,7 +36,10 @@ class Main_Screen(Screen):
         yield Placeholder("MAIN PAGE", id="main_page")
         yield Container(
             #Placeholder("CONTROLS", id="controls"),
-            
+            Current_Track(),
+            Button("Previous", id="Previous"),
+            Button("Play/Pause", id="Play"),
+            Button("Next", id="Next"),
             id="Bottom")
         yield Container(Current_Time_In_Track(),
                         Center(ProgressBar(total=100, id="bar", show_percentage=False, show_eta=False)), Track_Duration(),id="bar_container")
