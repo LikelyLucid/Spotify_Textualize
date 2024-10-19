e1b611961229ef84d76d0341204a30b9d92169ba
PurelyStupidity <micoolplays@gmail.com>
2024-10-19 07:13:18 +0000

    Oct 19, 2024, 8:13 PM

--- spotify_functions.py
+++ spotify_functions.py
@@ -28,3 +28,13 @@ if credentials == None:
 print(credentials)
 
 # sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+birdy_uri = "spotify:artist:2WX2uTcsvV5OnS0inACecP"
+
+results = sp.artist_albums(birdy_uri, album_type="album")
+albums = results["items"]
+while results["next"]:
+    results = sp.next(results)
+    albums.extend(results["items"])
+
+for album in albums:
+    print(album["name"])
