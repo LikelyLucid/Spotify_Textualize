0b14de9af2e12bbfe8241573e70cadb68958f156
PurelyStupidity <micoolplays@gmail.com>
2024-10-19 08:20:52 +0000

    Oct 19, 2024, 9:20 PM

--- spotify_functions.py
+++ spotify_functions.py
@@ -39,7 +39,7 @@ def authenticate_user():
     try:
         sp = spotipy.Spotify(
             auth_manager=SpotifyOAuth(
-                scope=scope,
+                # scope=scope,
                 client_id=client_id,
                 client_secret=client_secret,
                 redirect_uri=redirect_uri,
