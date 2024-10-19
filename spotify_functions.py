582e702ba5bf38c19fa4d6090fc9fe140d1b9e7b
PurelyStupidity <micoolplays@gmail.com>
2024-10-19 07:25:58 +0000

    Oct 19, 2024, 8:25 PM

--- spotify_functions.py
+++ spotify_functions.py
@@ -28,6 +28,7 @@ if credentials == None:
             client_id=client_id,
             client_secret=client_secret,
             redirect_uri=redirect_uri,
+            cache_handler="MemcacheCacheHandler",
         )
     )
 
