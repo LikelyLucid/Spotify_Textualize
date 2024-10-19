b5a3d1037106753640af82d3339272072ca3302c
PurelyStupidity <micoolplays@gmail.com>
2024-10-19 08:51:17 +0000

    Oct 19, 2024, 9:51 PM

--- main.py
+++ main.py
@@ -126,80 +126,80 @@ class Spotify_Playback_Data:
         print()
 
 
-# class Current_Time_In_Track(Widget):
-#     current_time = reactive("track_time")
+class Current_Time_In_Track(Widget):
+    current_time = reactive("track_time")
 
-#     def render(self) -> str:
-#         return "0:00"
-
-
-# class Track_Duration(Widget):
-#     track_duration = Spotify_Playback_Data().track_time
-
-#     def render(self) -> str:
-#         return "3:00"
+    def render(self) -> str:
+        return "0:00"
 
 
-# class Current_Track(Widget):
-#     current_track = reactive("track")
+class Track_Duration(Widget):
+    track_duration = Spotify_Playback_Data().track_time
 
-#     def render(self) -> str:
-#         return "TRACK PLACEHOLDER"
-
-
-# class Current_Volume(Widget):
-#     current_volume = reactive("volume")
-
-#     def render(self) -> str:
-#         return "VOLUME PLACEHOLDER"
+    def render(self) -> str:
+        return "3:00"
 
 
-# class Current_Device(Widget):
-#     current_device = reactive("device")
+class Current_Track(Widget):
+    current_track = reactive("track")
 
-#     def render(self) -> str:
-#         return "DEVICE PLACEHOLDER"
-
-
-# class Playing_Information(Widget):
-#     def render(self):
-#         # Return a string with playback info for the bottom bar
-#         return (
-#             "(Device: DEVICE PLACEHOLDER | Shuffle: OFF | Repeat: OFF | Volume: 100%)"
-#         )
+    def render(self) -> str:
+        return "TRACK PLACEHOLDER"
 
 
-# class Main_Screen(Screen):
-#     """The main page that contains:
-#     Main side bar
-#     Changelog
-#     Footer
-#     Playing bar"""
+class Current_Volume(Widget):
+    current_volume = reactive("volume")
 
-#     CSS_PATH = "main_page.tcss"
-
-#     def compose(self) -> ComposeResult:
-#         yield Placeholder("top_bar", id="top_bar")
-#         yield Placeholder("Spotify Stuff | Playlists", id="sidebar")
-#         yield Placeholder("Main Page", id="main_page")
-#         # yield Playing_Information(),
-#         yield Container(
-#             Current_Time_In_Track(),
-#             Center(
-#                 ProgressBar(total=100, id="bar", show_percentage=False, show_eta=False)
-#             ),
-#             Track_Duration(),
-#             id="bar_container",
-#         )
+    def render(self) -> str:
+        return "VOLUME PLACEHOLDER"
 
 
-# class MainApp(App):
-#     def on_mount(self) -> None:
-#         self.install_screen(Main_Screen(), "main")
-#         self.push_screen("main")
+class Current_Device(Widget):
+    current_device = reactive("device")
+
+    def render(self) -> str:
+        return "DEVICE PLACEHOLDER"
 
 
-# if __name__ == "__main__":
-#     app = MainApp()
-#     app.run()
+class Playing_Information(Widget):
+    def render(self):
+        # Return a string with playback info for the bottom bar
+        return (
+            "(Device: DEVICE PLACEHOLDER | Shuffle: OFF | Repeat: OFF | Volume: 100%)"
+        )
+
+
+class Main_Screen(Screen):
+    """The main page that contains:
+    Main side bar
+    Changelog
+    Footer
+    Playing bar"""
+
+    CSS_PATH = "main_page.tcss"
+
+    def compose(self) -> ComposeResult:
+        yield Placeholder("top_bar", id="top_bar")
+        yield Placeholder("Spotify Stuff | Playlists", id="sidebar")
+        yield Placeholder("Main Page", id="main_page")
+        # yield Playing_Information(),
+        yield Container(
+            Current_Time_In_Track(),
+            Center(
+                ProgressBar(total=100, id="bar", show_percentage=False, show_eta=False)
+            ),
+            Track_Duration(),
+            id="bar_container",
+        )
+
+
+class MainApp(App):
+    def on_mount(self) -> None:
+        self.install_screen(Main_Screen(), "main")
+        self.push_screen("main")
+
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.run()
 
