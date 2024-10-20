7a5e216a17614a4737aa14ac2728ab8921a21f5c
PurelyStupidity <micoolplays@gmail.com>
2024-10-20 12:00:36 +0000

    Oct 21, 2024, 1:00 AM

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ class Bottom_Bar(Widget):
         )
 
     def on_mount(self):
-        self.widget.styles.border = ("heavy", "yellow")
+        self.styles.border = ("heavy", "yellow")
 
 
 class Main_Screen(Screen):
