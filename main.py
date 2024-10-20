2ab02f9f16210f592657d37982e406d9aff51849
PurelyStupidity <micoolplays@gmail.com>
2024-10-20 11:35:41 +0000

    Oct 21, 2024, 12:35 AM

--- main.py
+++ main.py
@@ -75,9 +75,9 @@ class Playing_Information(Widget):
 
 class Bottom_Bar(Widget):
     def compose(self):
-        yield Current_Time_In_Track(),
+        yield Current_Time_In_Track(id = "current_time"),
         yield Center(ProgressBar(total=100, id="bar", show_percentage=False, show_eta=False)),
-        yield Track_Duration()
+        yield Track_Duration(id = "track_duration"),
 
 
 class Main_Screen(Screen):
