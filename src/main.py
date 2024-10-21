48d2f158b9340f18cd1d449dab8c6e31582cf5d0
PurelyStupidity <micoolplays@gmail.com>
2024-10-22 00:10:25 +1300

    12:10am

--- src/main.py
+++ src/main.py
@@ -3,12 +3,11 @@ from textual.binding import Binding
 from textual.screen import Screen
 from textual.widget import Widget
 from textual.containers import Container, Center, Middle, Horizontal, Vertical
-from textual.widgets import Footer, Placeholder, ProgressBar, Button
+from textual.widgets import Footer, Placeholder, ProgressBar, Button, Static
 from textual.reactive import reactive
 from spotify_main_class import Spotify_Playback_Data
 from textual import work
-from time import monotonic
-
+import time
 
 playback = (
     Spotify_Playback_Data()
@@ -30,17 +29,26 @@ def ms_to_time(ms: int) -> str:
     return f"{minutes}:{seconds:02d}"
 
 
+def get_current_time_with_offset() -> int:
+    """
+    Get the current playback time considering the offset.
+
+    Returns:
+    - The current time in milliseconds.
+    """
+    offset = int(time.time() * 1000) - playback.timestamp
+    print(f"Offset: {offset}")
+    return playback.progress_ms + offset
+
+
 class Current_Time_In_Track(Widget):
-    current_time = reactive(playback.progress_ms)
+    current_time = reactive(get_current_time_with_offset())
 
     def render(self) -> str:
         if playback.progress_ms is not None:
             return f"{ms_to_time(self.current_time)}"
         else:
             return ""
-
-    # def watch_current_time(self, current_time: int):
-    #     self.query_one(ProgressBar).update(progress=current_time)
 
 
 class Track_Duration(Widget):
@@ -74,21 +82,24 @@ class Current_Device(Widget):
         return self.current_device
 
 
-class Playing_Information(Widget):
-
-    def render(self):
-        # Return a string with playback info for the bottom bar
-        return (
-            f"{str(self)}"
-        )
-
-
 class Bottom_Bar(Widget):
-    # def __init__(self):
-    #     super().__init__()
+    def get_artist_info(self):
+        print("HIT")
+        if playback.track is not None:
+            artist_information = f"{playback.track} | "
+            for artist in playback.artists:
+                artist_information += artist + ", "
+            artist_information = artist_information[:-2]
+            print(str(artist_information))
+            return str(artist_information)
+        else:
+            return ""
 
     def compose(self):
+        info = self.get_artist_info()
+        print("WHAT THE FUCK")
         yield Container(
+            Static(f"{info}", id="artist_info"),
             Current_Time_In_Track(),
             Center(
                 ProgressBar(total=100, id="bar", show_percentage=False, show_eta=False)
@@ -97,13 +108,30 @@ class Bottom_Bar(Widget):
             id="bar_container",
         )
 
-    def update_progress(self):
-        self.query_one(Current_Time_In_Track).current_time += 1000
+    def update_progress(self, progress=None):
+        current_time_widget = self.query_one(Current_Time_In_Track)
+        if playback.is_playing and progress is None:
+            current_time_widget.current_time += 1000
+        else:
+            current_time_widget.current_time = progress
+        self.query_one(ProgressBar).update(
+            progress=current_time_widget.current_time,
+            total=playback.track_duration,
+        )
 
+    def song_change(self):
+        self.query_one(Track_Duration).track_duration = playback.track_duration
+        self.query_one(Current_Time_In_Track).current_time = (
+            get_current_time_with_offset()
+        )
+
+    def update_playback_settings(self):
+        self.border_title = playback.playing_settings()
+        self.update_progress(playback.progress_ms)
 
     def on_mount(self):
         self.styles.border = ("hkey", "blue")
-        self.border_title = playback.playing_settings()
+        self.update_playback_settings()
         self.set_interval(1, self.update_progress)
 
 
@@ -120,38 +148,25 @@ class Main_Screen(Screen):
         yield Placeholder("top_bar", id="top_bar")
         yield Placeholder("Spotify Stuff | Playlists", id="sidebar")
         yield Placeholder("Main Page", id="main_page")
-        # yield Playing_Information(),
-        # yield Container(
-        #     Current_Time_In_Track(),
-        #     Center(
-        #         ProgressBar(total=100, id="bar", show_percentage=False, show_eta=False)
-        #     ),
-        #     Track_Duration(),
-        #     id="bar_container",
-        # )
-        yield Bottom_Bar(id = "bottom_bar")
+        yield Bottom_Bar(id="bottom_bar")
+
+    async def update_stats(self):
+        old_song = playback.track
+        try:
+            playback.update()
+        except Exception as e:
+            print(f"Error updating playback data: {e}")
+            return
+        if playback.track is None:
+            return
+        elif old_song != playback.track:
+            if playback.is_playing:
+                self.query_one(Bottom_Bar).song_change()
+
+            self.query_one(Bottom_Bar).update_playback_settings()
 
     def on_mount(self) -> None:
-        def update_progress(current_time: int):
-            self.query_one(ProgressBar).update(
-                progress=current_time, total=playback.track_duration
-            )
-
-        # Song change
-        @work
-        async def update_stats():
-            old_song = playback.track
-            playback.update()
-            if playback.track is None:
-                return
-            elif old_song != playback.track:
-                self.post_message("update")
-
-
-
-        self.watch(
-            self.query_one(Current_Time_In_Track), "current_time", update_progress
-        )
+        self.set_interval(2, self.update_stats)
 
 
 class MainApp(App):
@@ -161,5 +176,5 @@ class MainApp(App):
 
 
 if __name__ == "__main__":
-    app = MainApp()
+    app = MainApp(ansi_color=True)
     app.run()
