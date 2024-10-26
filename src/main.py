508fc1215888630ed890ab939d4a9b8eecf65090
PurelyStupidity <micoolplays@gmail.com>
2024-10-26 06:43:09 +0000

    Oct 26, 2024, 7:43 PM

--- src/main.py
+++ src/main.py
@@ -152,11 +152,11 @@ class Library_List(Widget):
         self.library_data = library_data
         super().__init__(id=id)
 
-    def on_list_view_selected(self, item):
-        self.notify(f"item selected: {item.item.name}")
-        playlist_id = self.library_data[item.item.name]["id"]
+    def on_list_view_selected(self, selected_item):
+        self.notify(f"item selected: {selected_item.item.name}")
+        playlist_id = self.library_data[selected_item.item.id]["id"]
         for item in self.library_data:
-            if item["name"] == item.item.name:
+            if item["name"] == selected_item.item.name:
                 playlist_id = item["id"]
                 self.notify(f"playlist_id: {playlist_id}")
                 break
